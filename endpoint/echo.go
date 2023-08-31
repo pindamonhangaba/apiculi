e55752884ec2c7e467574eab66e95d28937a0ed1
Pedro Moraes <pindamonhangaba@users.noreply.github.com>
2023-08-31 17:55:02 -0300

    Add EchoWithContext options
    Allow the choice to prevent the request body from being consumed

--- endpoint/echo.go
+++ endpoint/echo.go
@@ -1,6 +1,8 @@
 package endpoint
 
 import (
+	"bytes"
+	"io"
 	"net/http"
 	"strings"
 
@@ -12,15 +14,31 @@ import (
 	"github.com/pkg/errors"
 )
 
-func EchoWithContext[C, P, Q, B any, D dataer](p endpointPath, d OpenAPIRouteDescriber, next EndpointWithContext[C, P, Q, B, D, echo.Context]) (string, string, echo.HandlerFunc) {
+type echoOptions struct {
+	restoreBody bool
+}
+
+func WithRestoredBody() echoOptions {
+	return echoOptions{
+		restoreBody: true,
+	}
+}
+
+func EchoWithContext[C, P, Q, B any, D dataer](p endpointPath, d OpenAPIRouteDescriber, next EndpointWithContext[C, P, Q, B, D, echo.Context], opts ...echoOptions) (string, string, echo.HandlerFunc) {
 	fillOpenAPIRoute[C, P, Q, B, D](endpointPath{
 		verb: p.verb,
 		path: routerPathToOpenAPIPath(p.path),
 	}, d)
 
+	defaultOptions := echoOptions{}
+
+	for _, opt := range opts {
+		defaultOptions.restoreBody = opt.restoreBody
+	}
+
 	return string(p.verb), p.path, func(c echo.Context) error {
 
-		cc, prs, q, b, err := parseBodyEcho[C, P, Q, B, D](p, c)
+		cc, prs, q, b, err := parseBodyEcho[C, P, Q, B, D](p, c, defaultOptions.restoreBody)
 		if err != nil {
 			return err
 		}
@@ -51,7 +69,7 @@ func Echo[C, P, Q, B any, D dataer](p endpointPath, d OpenAPIRouteDescriber, nex
 
 	return string(p.verb), p.path, func(c echo.Context) error {
 
-		cc, prs, q, b, err := parseBodyEcho[C, P, Q, B, D](p, c)
+		cc, prs, q, b, err := parseBodyEcho[C, P, Q, B, D](p, c, false)
 		if err != nil {
 			return err
 		}
@@ -73,7 +91,7 @@ func Echo[C, P, Q, B any, D dataer](p endpointPath, d OpenAPIRouteDescriber, nex
 	}
 }
 
-func parseBodyEcho[C, P, Q, B any, D dataer](p endpointPath, c echo.Context) (cc C, prs P, q Q, b *B, err error) {
+func parseBodyEcho[C, P, Q, B any, D dataer](p endpointPath, c echo.Context, restoreBody bool) (cc C, prs P, q Q, b *B, err error) {
 	if _, ok := Find([]string{http.MethodGet, http.MethodConnect, http.MethodHead, http.MethodTrace, http.MethodOptions}, string(p.verb)); !ok {
 		contt := strings.Split(c.Request().Header.Get("Content-Type"), ";")[0]
 		switch contt {
@@ -116,13 +134,23 @@ func parseBodyEcho[C, P, Q, B any, D dataer](p endpointPath, c echo.Context) (cc
 	if err != nil {
 		return cc, prs, q, b, errors.Wrap(err, "query")
 	}
-
+	origBody := []byte{}
+	if restoreBody {
+		origBody, err = io.ReadAll(c.Request().Body)
+		if err != nil {
+			return cc, prs, q, b, errors.Wrap(err, "duplicating body")
+		}
+		c.Request().Body = io.NopCloser(bytes.NewBuffer(origBody))
+	}
 	b = new(B)
 	if has([]httpVerb{PUT, POST, DELETE, PATCH}, p.verb) {
 		err = c.Bind(b)
 		if err != nil {
 			return cc, prs, q, b, errors.Wrap(err, "body")
 		}
+	}
+	if restoreBody {
+		c.Request().Body = io.NopCloser(bytes.NewBuffer(origBody))
 	}
 
 	return cc, prs, q, b, nil
